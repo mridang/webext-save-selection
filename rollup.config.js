886beeb765d782aab3ab7cf51144e96c7f0f9efb
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-12-14 20:01:49 +1100

    feat: initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,29 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import copy from 'rollup-plugin-copy';
+import { Resvg } from '@resvg/resvg-js';
+import { readFileSync, writeFileSync, mkdirSync } from 'node:fs';
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
 
 const execAsync = promisify(exec);
+
+// noinspection JSUnusedGlobalSymbols
+const convertSvgPlugin = {
+  name: 'convert-svg',
+  buildStart() {
+    const svg = readFileSync('icon.svg', 'utf-8');
+    mkdirSync('dist/icons', { recursive: true });
+
+    for (const size of [16, 48, 128]) {
+      const resvg = new Resvg(svg, {
+        fitTo: { mode: 'width', value: size },
+      });
+      const png = resvg.render().asPng();
+      writeFileSync(`dist/icons/icon-${size}.png`, png);
+    }
+  },
+};
 
 // noinspection JSUnusedGlobalSymbols
 const zipPlugin = () => ({
@@ -40,11 +59,9 @@ export default {
     terser({
       sourceMap: true,
     }),
+    convertSvgPlugin,
     copy({
-      targets: [
-        { src: 'manifest.json', dest: 'dist' },
-        { src: 'icons', dest: 'dist' },
-      ],
+      targets: [{ src: 'manifest.json', dest: 'dist' }],
     }),
     zipPlugin(),
   ],
